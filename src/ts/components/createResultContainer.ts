956cffb4919e37bc645db0ec15cba1489e25a3e7
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-04-07 19:52:56 +0200

    Fixed the issue when hsl color has been written to history and couldn't be read after

--- src/ts/components/createResultContainer.ts
+++ src/ts/components/createResultContainer.ts
@@ -1,43 +1,43 @@
-import colorHistory from "./colorHistory";
+import { colorHistory } from './colorHistory';
 
 const clipboardSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" class="clipboard-svg"><path d="M19 3h-2.25a1 1 0 0 0-1-1h-7.5a1 1 0 0 0-1 1H5c-1.103 0-2 .897-2 2v15c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zm0 17H5V5h2v2h10V5h2v15z"></path></svg>`;
 
 const checkOutSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" class="clipboard-svg"><path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></path></svg>`;
 
 function createColorTable(colorArray: string[]) {
-  const colorHistoryContainer = document.createElement("div");
-  colorHistoryContainer.className = "fresh-baked__history-container";
+  const colorHistoryContainer = document.createElement('div');
+  colorHistoryContainer.className = 'fresh-baked__history-container';
 
-  const h3 = document.createElement("h3");
-  h3.className = "fresh-baked__history-h3";
-  h3.innerText = "Colors History";
+  const h3 = document.createElement('h3');
+  h3.className = 'fresh-baked__history-h3';
+  h3.innerText = 'Colors History';
 
-  const colorTable = document.createElement("table");
-  colorTable.className = "fresh-baked__history-table";
+  const colorTable = document.createElement('table');
+  colorTable.className = 'fresh-baked__history-table';
 
-  const headers = document.createElement("tr");
-  headers.className = "fresh-baked__history-table-row--header";
+  const headers = document.createElement('tr');
+  headers.className = 'fresh-baked__history-table-row--header';
 
   for (let i = -2; i < colorArray.length; i++) {
     if (i < 0) {
-      const th = document.createElement("th");
-      th.className = "fresh-baked__history-table-header";
-      th.innerText = i === -2 ? "Preview" : "Format";
+      const th = document.createElement('th');
+      th.className = 'fresh-baked__history-table-header';
+      th.innerText = i === -2 ? 'Preview' : 'Format';
       headers.append(th);
       continue;
     }
 
     if (!i) colorTable.append(headers);
     // binding colors
-    const tr = document.createElement("tr");
-    tr.className = "fresh-baked__history-table-row";
+    const tr = document.createElement('tr');
+    tr.className = 'fresh-baked__history-table-row';
 
-    const previewBox = document.createElement("div");
-    previewBox.className = "fresh-baked__history-color-preview";
+    const previewBox = document.createElement('div');
+    previewBox.className = 'fresh-baked__history-color-preview';
     previewBox.style.backgroundColor = colorArray[i];
 
-    const outputFormat = document.createElement("p");
-    outputFormat.className = "fresh-baked__history-color-text";
+    const outputFormat = document.createElement('p');
+    outputFormat.className = 'fresh-baked__history-color-text';
     outputFormat.innerText = colorArray[i];
 
     tr.append(previewBox);
@@ -61,16 +61,16 @@ export function createResultContainer(
   if (colorForm.nextElementSibling) {
     const existingSection = colorForm.nextElementSibling,
       outputElement = existingSection.querySelector<HTMLParagraphElement>(
-        ".fresh-baked__output-element"
+        '.fresh-baked__output-element'
       ),
       colorPreviewContainer = existingSection.querySelector<HTMLDivElement>(
-        ".fresh-baked__color-preview"
+        '.fresh-baked__color-preview'
       ),
       pasteToClipboardButton = existingSection.querySelector<HTMLButtonElement>(
-        ".fresh-baked__clipboard-button"
+        '.fresh-baked__clipboard-button'
       ),
       colorHistoryContainer = existingSection.querySelector<HTMLDivElement>(
-        ".fresh-baked__history-container"
+        '.fresh-baked__history-container'
       );
 
     colorPreviewContainer!.style.backgroundColor = newColor;
@@ -88,27 +88,27 @@ export function createResultContainer(
 
   const justBakedContainer = new DocumentFragment();
 
-  const section = document.createElement("section");
-  section.classList.add("fresh-baked");
+  const section = document.createElement('section');
+  section.classList.add('fresh-baked');
 
-  const h2 = document.createElement("h2");
-  h2.innerText = "Here, this is your new color! 😊";
+  const h2 = document.createElement('h2');
+  h2.innerText = 'Here, this is your new color! 😊';
 
-  const div = document.createElement("div");
-  div.classList.add("fresh-baked__block-label");
+  const div = document.createElement('div');
+  div.classList.add('fresh-baked__block-label');
 
-  const colorPreviewContainer = document.createElement("div");
-  colorPreviewContainer.classList.add("fresh-baked__color-preview");
+  const colorPreviewContainer = document.createElement('div');
+  colorPreviewContainer.classList.add('fresh-baked__color-preview');
   colorPreviewContainer.style.backgroundColor = newColor;
 
-  const outputElement = document.createElement("p");
-  outputElement.classList.add("fresh-baked__output-element");
+  const outputElement = document.createElement('p');
+  outputElement.classList.add('fresh-baked__output-element');
   outputElement.innerText = newColor;
 
-  const pasteToClipboardButton = document.createElement("button");
+  const pasteToClipboardButton = document.createElement('button');
   pasteToClipboardButton.innerHTML = clipboardSVG;
-  pasteToClipboardButton.classList.add("fresh-baked__clipboard-button");
-  pasteToClipboardButton.dataset.tooltip = "Saved to your clipboard!";
+  pasteToClipboardButton.classList.add('fresh-baked__clipboard-button');
+  pasteToClipboardButton.dataset.tooltip = 'Saved to your clipboard!';
   // button.onclick
   listenForClick(pasteToClipboardButton, newColor);
 
@@ -134,14 +134,14 @@ export function listenForClick(
     pasteToClipboardButton.innerHTML = checkOutSVG;
     const animationTime = 2500;
     pasteToClipboardButton.style.setProperty(
-      "--animation-duration",
+      '--animation-duration',
       `${animationTime}ms`
     );
-    pasteToClipboardButton.style.setProperty("--animation-name", "fade-in-out");
+    pasteToClipboardButton.style.setProperty('--animation-name', 'fade-in-out');
 
     setTimeout(() => {
-      pasteToClipboardButton.style.removeProperty("--animation-duration");
-      pasteToClipboardButton.style.removeProperty("--animation-name");
+      pasteToClipboardButton.style.removeProperty('--animation-duration');
+      pasteToClipboardButton.style.removeProperty('--animation-name');
     }, animationTime);
   };
 }
